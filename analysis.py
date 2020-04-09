5f93b7d6f1fb3a35e39e655112a0c8fde5793e94
Skylark <24936522+Skylark0924@users.noreply.github.com>
2020-04-09 23:16:54 +0800

    R to py convert

--- analysis.py
+++ analysis.py
@@ -3,29 +3,26 @@ from nltk.corpus import wordnet as wn
 import nltk
 from rpy2 import robjects
 
-r=robjects.r
+r = robjects.r
 
-# r['library']('networkD3')
-# r['data']('MisLinks')
-# r['data']('MisNodes')
-
-
-str="""
+str = """
 library(networkD3)
 data(MisLinks)
 data(MisNodes)
-forceNetwork(Links = MisLinks, Nodes = MisNodes,
+a=forceNetwork(Links = MisLinks, Nodes = MisNodes,
              Source = "source", Target = "target",
              Value = "value", NodeID = "name",
              Group = "group", opacity = 0.8)
-             """
+saveNetwork(a, 'D:/Github/Vocabulary_Analysis/net2.html')
+"""
 
 r(str)
 
 # corpus = pd.read_csv('./ECDICT/ecdict.csv', sep=',', error_bad_lines=False, index_col=False, dtype='unicode')
+
+
 def print_all_lemma(word):
     for i in wn.synsets(word):
         w = i
         print('{}'.format(w), w.lemma_names())
-
 
--- dyna_relation_map.R
+++ dyna_relation_map.R
@@ -16,7 +16,8 @@ data(MisLinks)
 data(MisNodes)
 
 # Plot
-forceNetwork(Links = MisLinks, Nodes = MisNodes,
+a=forceNetwork(Links = MisLinks, Nodes = MisNodes,
              Source = "source", Target = "target",
              Value = "value", NodeID = "name",
              Group = "group", opacity = 0.8)
+saveNetwork(a, 'D:/Github/Vocabulary_Analysis/net.html')
